9a2ce8f0922d4d1d2602c7ef99ac3e4e0e05cee9
lgxoyx <lgxoyx@outlook.com>
2016-12-07 17:10:44 +0800

    1207
    V0.01

---
+++ js/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created by li on 2016/12/7.
+ */
+//  轮播图插件的应用
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay: 5000,//可选选项，自动滑动
+  loop: true,
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+});
+//  轮播图插件的应用
